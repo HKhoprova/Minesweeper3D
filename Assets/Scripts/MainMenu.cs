b17d073e0d8a7358ca123e9eb06938af9ff32724
HKhoprova <jackiedaw314@hotmail.com>
2025-01-18 00:59:04 +0100

    Finished tutorial
    Added changing pictures in tutorial.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject images;
+    [SerializeField] private float interval = 1.5f;
+
+    private bool isVisible = true;
+
+    private void Start()
+    {
+        if (images == null)
+        {
+            images = gameObject;
+        }
+        InvokeRepeating(nameof(ToggleVisibility), interval, interval);
+    }
+
+    private void ToggleVisibility()
+    {
+        isVisible = !isVisible;
+        images.SetActive(isVisible);
+    }
+
     public void PlaySquare()
     {
         LevelManager.Instance.LoadSquareLevel();
